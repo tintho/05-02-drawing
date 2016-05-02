7837b3717e7f2603bd7cdd4e1559b9cc5e38cdb2
Joel Ross <joelross@uw.edu>
2016-05-01 20:09:08 -0700

    Add starting lecture code

---
+++ app/src/main/java/edu/uw/drawdemo/ButtonActivity.java
@@ -0,0 +1,30 @@
+package edu.uw.drawdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    private static final String TAG = "Button";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        final Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Clicked!");
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/uw/drawdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.uw.drawdemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    /** Menus **/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.menu_button:
+                startActivity(new Intent(MainActivity.this, ButtonActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
